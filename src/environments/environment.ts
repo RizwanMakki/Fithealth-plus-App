af9670f8af762596f7829bd66c5d615fee11f3bc
RizwanMakki <rizwanarifmakki@gmail.com>
2025-07-31 20:52:09 +0500

    Added new standalone Angular + Ionic project

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+firebaseConfig : {
+  apiKey: "AIzaSyA5BgKuiua1wJa26DWx_Hf_jpZC1vOEChs",
+  authDomain: "fithealth-plus.firebaseapp.com",
+  projectId: "fithealth-plus",
+  storageBucket: "fithealth-plus.firebasestorage.app",
+  messagingSenderId: "90779443459",
+  appId: "1:90779443459:web:580ee2fdf83f83552a8936",
+  measurementId: "G-BGRG2316Y0"
+}
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
